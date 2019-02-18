6c9f73a9b4167aae86771820b44d101a6a03e395
Anatolii Silinskyi <silinskyi92@gmail.com>
2019-02-18 03:56:55 +0200

    added Grunt, event

--- index.js
+++ index.js
@@ -2,15 +2,35 @@ const gm = require('gm').subClass({imageMagick: true});
 const s3 = require('./controllers/s3')
 
 
-s3.getObject("ansil-bucket", "cat.jpg")
+exports.handler = (event, context) => {
+
+    // Source Bucket
+    const srcBucket = event.Records[0].s3.bucket.name;
+
+    // Dst Bucket
+    const dstBucket = `${srcBucket}-resize`
+
+    // Key
+    const key = event.Records[0].s3.object.key
+
+    s3.getObject(srcBucket, key)
     .then((data) => {
         gm(data.Body)
         .resize(100, 100)
-        .toBuffer((err, buffer) => {
-        if (err) {
-            console.log(err);
-        } else {
-            s3.putObject(data, "ansil-bucket-resize", "cat-resize.jpg")
-        }
+        .toBuffer((err, data) => {
+            if (err) {
+                context.fail(err);
+            } else {
+                s3.putObject(data, dstBucket, key)
+                    .then(() => {
+                        context.done(null, "Complete")
+                    })
+                    .catch((err) => {
+                        context.fail(err);
+                    })
+            }
+        })
     })
+    .catch((err) => {
+        context.fail(err);
     })
