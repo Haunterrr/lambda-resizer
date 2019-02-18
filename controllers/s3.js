7990176a8ab749fcc8d9abc74aa8e98a0115bc65
Anatolii Silinskyi <silinskyi92@gmail.com>
2019-02-18 02:45:49 +0200

    added putObject func

---
+++ controllers/s3.js
@@ -0,0 +1,30 @@
+const AWS = require('aws-sdk')
+
+AWS.config.apiVersion = {
+    s3: '2006-03-01',
+};
+
+AWS.config.update({region: 'eu-central-1'});
+
+const s3 = new AWS.S3();
+
+const putObject = (body, bucket, key) => {
+    return new Promise((resolve, reject) => {
+        const params = {
+            Body: body,
+            Bucket: bucket,
+            Key: key,
+        };
+
+        s3.putObject(params, (err, data) => {
+            if (err) reject(err);
+            else {
+                resolve(data)
+            }
+        });
+    })    
+}
+
+module.exports = {
+    putObject
+}
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const gm = require('gm').subClass({imageMagick: true});
+const s3 = require('./controllers/s3')
 
-gm('img.jpg')
+
+gm('cat.jpg')
     .resize(100, 100)
     .toBuffer((err, buffer) => {
-    if (err) return handle(err);
-    console.log('done!');
-}
+    if (err) {
+        console.log(err);
+    } else {
+        s3.putObject(buffer, "ansil-bucket-resize", "cat-resize.jpg")
+    }
+})
